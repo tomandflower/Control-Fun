6787547062f9e823b3b6512b2e2d510c2ff6c233
apple <apple@appletekiMacBook-Air-2.local>
2015-01-29 13:35:35 +0800

    Initial Commit

---
+++ Control Fun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Control Fun
+//
+//  Created by apple on 15/1/29.
+//  Copyright (c) 2015年 apple.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
